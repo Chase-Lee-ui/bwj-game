def284fa9dc568fc612119dbaa160da499b34b17
Hoobot <73650949+Hooobot@users.noreply.github.com>
2022-04-03 00:50:44 -1000

    Added Timer
    also fixed some prior text issues

---
+++ bwj-game/Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] public float timeValue = 60;
+    public Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+            //outa energy death / tentative to change
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        DisplayTime(timeValue);
+    }
+    
+    void DisplayTime(float currentTime)
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

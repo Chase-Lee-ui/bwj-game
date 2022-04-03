582870abac61baa1b73321574c7d66a17b3d5d01
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-04-02 16:25:29 -1000

    progress push

---
+++ bwj-game/Assets/Scripts/Task_Things/Task_Progress.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task_Progress : MonoBehaviour
+{
+    public float Progress = 0;
+    public float Rate_Of_Decay = 0.5f;
+
+    public void Update()
+    {
+        if(Progress >= 0)
+        {
+            Progress -= Rate_Of_Decay * Time.deltaTime;
+        }
+    }
+}

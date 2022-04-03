c924d86f720b2dfd2a2a9e92da40281107c0fea0
Nathan <96511258+Le1337n00b@users.noreply.github.com>
2022-04-03 13:41:19 -1000

    task list WIP

---
+++ bwj-game/Assets/Scripts/ListManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Task_Progress task in Task_Progresses)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

e44dc6508ccd70137d566d958f3645e07ffc6e32
Nathan <96511258+Le1337n00b@users.noreply.github.com>
2022-04-03 14:38:16 -1000

    list manager WIP

--- bwj-game/Assets/Scripts/ListManager.cs
+++ bwj-game/Assets/Scripts/ListManager.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ListManager : MonoBehaviour
 {
+    [SerializeField] GameObject taskEntryPrefab;
+
+    private GameObject manager;
+
+    [SerializeField] LinkedList<GameObject> taskList;
+
     // Start is called before the first frame update
     void Start()
-    {/*
-        foreach (Task_Progress task in Task_Progresses)
+    {
+        manager = GameObject.Find("Manager");
+
+        foreach (Task_Progress task in manager.GetComponent<Task_Manager>().Task_Progresses)
         {
-            
-        }*/
+            GameObject tTask = Instantiate(taskEntryPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+            tTask.name = task.NameOfTask;
+            tTask.GetComponent<TMP_Text>().text = task.NameOfTask;
+
+            taskList.AddLast(tTask);
+        }
     }
 
     // Update is called once per frame

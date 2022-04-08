64dd3f33ca25273bcd5ee5f7abdd9c4e69de176e
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-04-07 19:36:24 -1000

    mini push

--- bwj-game/Assets/Scripts/Task_Things/TASK_Laundry.cs
+++ bwj-game/Assets/Scripts/Task_Things/TASK_Laundry.cs
@@ -2,22 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TASK_Laundry : Task_Progress
+public class Task_Laundry : Task_Progress
 {
+    public int LaundryIN = 0;
+    public int AmtOfLaundry = 3;
+    public GameObject Laundry;
+    public GameObject Canvas;
+    public bool Spawned;
     // Start is called before the first frame update
     void Start()
     {
+        NameOfTask = "Laundry";
+    }
+
+    public override void Update()
+    {
+        if(!Spawned)
+        {
+            for(int i = 0; i<AmtOfLaundry; i++)
+            {
+                var img = Instantiate(Laundry) as GameObject;
+                img.transform.SetParent(Canvas.transform, false);
+            }
+            Spawned = true;
+        }
         
     }
 
-    // Update is called once per frame
-    // public override void Update()
-    // {
-        
-    // }
-
-    private void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
-        // if(col.)
+        if(col.gameObject.tag == "Laundry")
+        {
+            LaundryIN++;
+        }
     }
 }

817c40be4b20926a8b8d1db8dff4c46b058a4956
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2022-04-07 20:42:58 -1000

    mini push

--- bwj-game/Assets/Scripts/Task_Things/TASK_Laundry.cs
+++ bwj-game/Assets/Scripts/Task_Things/TASK_Laundry.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Task_Laundry : Task_Progress
+public class TASK_Laundry : Task_Progress
 {
-    public int LaundryIN = 0;
+    public Laundry_Collect Collector;
+    private int LaundryIN = 0;
     public int AmtOfLaundry = 3;
     public GameObject Laundry;
     public GameObject Canvas;
@@ -21,12 +22,13 @@ public class Task_Laundry : Task_Progress
         {
             for(int i = 0; i<AmtOfLaundry; i++)
             {
-                var img = Instantiate(Laundry) as GameObject;
+                var img = Instantiate(Laundry, Canvas.transform) as GameObject;
                 img.transform.SetParent(Canvas.transform, false);
             }
             Spawned = true;
         }
-        
+        LaundryIN = Collector.Laundry_Inside;
+        Progress = LaundryIN / AmtOfLaundry;
     }
 
     void OnTriggerEnter2D(Collider2D col)

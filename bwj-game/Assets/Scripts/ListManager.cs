b47b9e601a8322f8d73a39f42bc9914f79b31c0e
Nathan <96511258+Le1337n00b@users.noreply.github.com>
2022-04-10 13:43:43 -1000

    list manager fix

--- bwj-game/Assets/Scripts/ListManager.cs
+++ bwj-game/Assets/Scripts/ListManager.cs
@@ -5,25 +5,41 @@ using TMPro;
 
 public class ListManager : MonoBehaviour
 {
-    [SerializeField] GameObject taskEntryPrefab;
+    [SerializeField] private GameObject taskEntryPrefab;
 
     private GameObject manager;
 
-    [SerializeField] LinkedList<GameObject> taskList;
+    [SerializeField] private List<GameObject> taskList;
 
     // Start is called before the first frame update
     void Start()
     {
+        taskList = new List<GameObject>();
+
         manager = GameObject.Find("Manager");
 
-        foreach (Task_Progress task in manager.GetComponent<Task_Manager>().Task_Progresses)
+        if (manager.GetComponent<Task_Manager>().Task_Progresses.Length < 1)
         {
             GameObject tTask = Instantiate(taskEntryPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            tTask.transform.SetParent(transform, false);
 
-            tTask.name = task.NameOfTask;
-            tTask.GetComponent<TMP_Text>().text = task.NameOfTask;
+            tTask.name = "Placeholder";
+            tTask.GetComponent<TMP_Text>().text = "Placeholder";
 
-            taskList.AddLast(tTask);
+            taskList.Add(tTask);
+        }
+        else
+        {
+            foreach (Task_Progress task in manager.GetComponent<Task_Manager>().Task_Progresses)
+            {
+                GameObject tTask = Instantiate(taskEntryPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                tTask.transform.SetParent(transform, false);
+
+                tTask.name = task.NameOfTask;
+                tTask.GetComponent<TMP_Text>().text = task.NameOfTask;
+
+                taskList.Add(tTask);
+            }
         }
     }
 

2f01f4bce80db6191a595d9cef3310f5b82847a0
Hoobot <73650949+Hooobot@users.noreply.github.com>
2022-04-10 15:30:52 -1000

    cookign done

--- bwj-game/Assets/Scripts/Task_Things/Task_Cooking.cs
+++ bwj-game/Assets/Scripts/Task_Things/Task_Cooking.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Task_Cooking : MonoBehaviour
+public class Task_Cooking : Task_Progress
 {
     [SerializeField] public float progressRate;
     public GameObject FryingPan;
@@ -11,7 +11,6 @@ public class Task_Cooking : MonoBehaviour
     public Slider slider;
 
     public float currentProgress;
-    public float progress;
 
     public Cooking_Progress within;
     
@@ -22,122 +21,32 @@ public class Task_Cooking : MonoBehaviour
         
     }
 
-    void Update()
+    public override void Update()
     {
-        /*
-        if (FryingPan.gameObject.transform.position.y <= 195
-            && FryingPan.gameObject.transform.position.x <= -65
-            && FryingPan.gameObject.transform.position.y >= 145
-            && FryingPan.gameObject.transform.position.x >= -200)
-        {
-            progress += Time.deltaTime*progressRate;
-            slider.value = progress;
-        }
-        */
         if (within.cooking)
         {
-            progress += Time.deltaTime * progressRate;
-            slider.value = progress;
+            Progress += Time.deltaTime * progressRate;
+            slider.value = Progress;
         }
         else
         {
-            if (progress > 0)
+            if (Progress > 0)
             {
-                progress -= Time.deltaTime * progressRate / 2;
-                slider.value = progress;
+                Progress -= Time.deltaTime * progressRate / 2;
+                slider.value = Progress;
             }
         }
-
-        //progress += Time.deltaTime * progressRate;
-        slider.value = progress;
+        /*
+        slider.value = Progress;
         if (Input.GetKeyDown(KeyCode.R))
         {
             slider.value = 0;
-            progress = 0;
+            Progress = 0;
         }
+        */
     }
     public void completionRate(float progress)
     {
         slider.value = progress;
     }
 }
-//failed drag and drop
-    /*
-    private bool isDragging;
-
-    public void OnMouseDown()
-    {
-        isDragging = true;
-    }
-
-    public void OnMouseUp()
-    {
-        isDragging = false;
-    }
-
-    void Update()
-    {
-        if (isDragging)
-        {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.Translate(mousePosition);
-        }
-    }
-    */
-//for knobs, canned from other image
-    /*public GameObject image;
-    public GameObject image2;
-    public GameObject image3;
-    public GameObject image4;
-    public Button button;
-    int wincondition = 0;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (image.gameObject.activeSelf && !image4.gameObject.activeSelf)
-        {
-            button.onClick.AddListener(() => {
-                image.SetActive(false);
-                image2.SetActive(true);
-            });
-        }
-        if (image2.gameObject.activeSelf && !image.gameObject.activeSelf)
-        {
-            button.onClick.AddListener(() => {
-                image2.SetActive(false);
-                image3.SetActive(true);
-            });
-        }
-        if (image3.gameObject.activeSelf && !image2.gameObject.activeSelf)
-        {
-            button.onClick.AddListener(() => {
-                image3.SetActive(false);
-                image4.SetActive(true);
-            });
-        }
-        if (image4.gameObject.activeSelf && !image3.gameObject.activeSelf)
-        {
-            button.onClick.AddListener(() => {
-                image4.SetActive(false);
-                image.SetActive(true);
-            });
-        }
-        if (image4.gameObject.activeSelf)
-        {
-            wincondition = 1;
-        }
-        if (wincondition == 1)
-        {
-            //button.gameObject.SetActive(false);
-        }
-    }
-    */
-
